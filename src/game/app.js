28b8e44ec14e760f7295e8010f1476102555b9cd
Juan Araujo <araujogreydiego@gmail.com>
2021-12-21 13:21:48 -0400

    Changes code formatting over app.js

--- src/game/app.js
+++ src/game/app.js
@@ -2,29 +2,29 @@ new Vue({
   el: "#app",
   data: {
     health: 100,
-    ended: false
-  },
-  created() {
-    window.addEventListener('keydown', (e) => {
-      if (e.key === ' ' | e.key === 'Enter') {
-        this.health -= 10
-        if (this.health === 0 ) {
-          this.ended = true;
-        }
-      }
-    })
+    ended: false,
   },
   methods: {
     punch() {
       this.health -= 10;
-      if (this.health === 0 ) {
+      if (this.health === 0) {
         this.ended = true;
       }
     },
     restart() {
       this.health = 100;
       this.ended = false;
-    }
+    },
   },
   computed: {},
-})
+  created() {
+    window.addEventListener("keydown", (e) => {
+      if ((e.key === " ") | (e.key === "Enter")) {
+        this.health -= 10;
+        if (this.health === 0) {
+          this.ended = true;
+        }
+      }
+    });
+  },
+});
